f3f349d9ae7ed10fe807d8dad66430bd3d235b6d
kovalev Andrey <andremae10@gmail.com>
2024-09-30 09:09:39 +0300

    cipher module, dont change letters that not in alphabet and lowercase result

--- src/modules/substitution_ciphers.rs
+++ src/modules/substitution_ciphers.rs
@@ -4,15 +4,22 @@ pub fn caesars(text: String, alphabet: &str, shift: i32) -> String {
     let length = alphabet.chars().count();
     let mut res = String::new();
 
-    for letter in text.chars() {
-        let index = alphabet.find(letter).ok_or(0).unwrap_or(0);
-        res.push(
-            alphabet
-                .chars()
-                .nth(((index as i32) + shift).rem_euclid(length as i32) as usize)
-                .ok_or("")
-                .unwrap()
-        );
+    for letter in text.to_lowercase().chars() {
+        let index = alphabet.graphemes(true).position(|x| x == &letter.to_string());
+        match index {
+            Some(idx) => {
+                res.push(
+                    alphabet
+                        .chars()
+                        .nth(((idx as i32) + shift).rem_euclid(length as i32) as usize)
+                        .ok_or("")
+                        .unwrap()
+                );
+            }
+            None => {
+                res.push(letter);
+            }
+        }
     }
 
     res
@@ -36,19 +43,22 @@ pub fn trisemus(text: String, alphabet: String, key_word: &str, cols: i32) -> St
         }
     }
 
-    for letter in text.chars() {
-        let index = table
-            .graphemes(true)
-            .position(|x| x == &letter.to_string())
-            .ok_or(0)
-            .unwrap_or(0);
-        res.push(
-            table
-                .chars()
-                .nth(((index as i32) + cols).rem_euclid(length as i32) as usize)
-                .ok_or("")
-                .unwrap()
-        );
+    for letter in text.to_lowercase().chars() {
+        let index = table.graphemes(true).position(|x| x == &letter.to_string());
+        match index {
+            Some(idx) => {
+                res.push(
+                    table
+                        .chars()
+                        .nth(((idx as i32) + cols).rem_euclid(length as i32) as usize)
+                        .ok_or("")
+                        .unwrap()
+                );
+            }
+            None => {
+                res.push(letter);
+            }
+        }
     }
 
     res
