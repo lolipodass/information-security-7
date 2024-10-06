e27de8d4d5b49b448c9fa00e9b23bee5c3f0c586
kovalev Andrey <andremae10@gmail.com>
2024-10-06 13:06:55 +0300

    feat(benches): add trisemus benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,6 @@
 extern crate criterion;
 use criterion::{ black_box, criterion_group, criterion_main, Criterion };
-use primeculator::modules::substitution_ciphers::caesars;
+use primeculator::modules::substitution_ciphers::{ caesars, trisemus };
 
 fn benchmark_caesars(c: &mut Criterion) {
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -24,6 +24,29 @@ fn benchmark_caesars(c: &mut Criterion) {
     }
     group.finish();
 }
+fn benchmark_trisemus(c: &mut Criterion) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let shift = 4;
 
-criterion_group!(benches, benchmark_caesars);
+    let sizes = vec![10, 100, 1000, 10000, 100000];
+
+    let mut group = c.benchmark_group("trisemus");
+    for &size in &sizes {
+        let text = "A".repeat(size);
+        group.bench_function(format!("trisemus_{}", size), |b| {
+            b.iter(|| {
+                let result = trisemus(
+                    black_box(text.clone()),
+                    black_box(alphabet.to_string()),
+                    black_box("enigma"),
+                    black_box(shift)
+                );
+                black_box(result);
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, benchmark_caesars, benchmark_trisemus);
 criterion_main!(benches);
