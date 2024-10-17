ae3fde992123e1b9964faffc0126d994d7b2dd7f
kovalev Andrey <andremae10@gmail.com>
2024-10-17 23:41:52 +0300

    feat(lab4): add ui for enigma function

--- src/view/lab4.rs
+++ src/view/lab4.rs
@@ -1,8 +1,14 @@
+use egui::DragValue;
+
+use crate::modules::enigma::enigma_cipher::enigma_cipher;
+
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct EnigmaCipher {
     input: String,
     result: String,
+    positions: (i32, i32, i32),
+    rotations: (i32, i32, i32),
 }
 
 impl Default for EnigmaCipher {
@@ -10,6 +16,8 @@ impl Default for EnigmaCipher {
         Self {
             input: "empty!".to_owned(),
             result: "Result".to_owned(),
+            positions: (0, 0, 0),
+            rotations: (0, 0, 0),
         }
     }
 }
@@ -28,7 +36,29 @@ impl EnigmaCipher {
 
         ui.text_edit_singleline(&mut self.input);
 
+        ui.horizontal(|ui| {
+            ui.label("positions: ");
+            ui.add(
+                DragValue::new(&mut self.positions.0)
+                    .speed(1)
+                    .range(0..=25)
+            );
+            ui.add(DragValue::new(&mut self.positions.1).range(0..=25));
+            ui.add(DragValue::new(&mut self.positions.2).range(0..=25));
+        });
+
+        ui.horizontal(|ui| {
+            ui.label("rotations: ");
+            ui.add(
+                DragValue::new(&mut self.rotations.0)
+                    .speed(1)
+                    .range(0..=25)
+            );
+            ui.add(DragValue::new(&mut self.rotations.1).range(0..=25));
+            ui.add(DragValue::new(&mut self.rotations.2).range(0..=25));
+        });
         if ui.button("compute").clicked() {
+            self.result = enigma_cipher(self.input.clone(), self.rotations, self.positions);
         }
 
         ui.separator();
