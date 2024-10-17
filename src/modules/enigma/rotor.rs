dc927be5a25e7510600bc41911990c3ca32ed028
kovalev Andrey <andremae10@gmail.com>
2024-10-17 23:40:08 +0300

    feat(enigma rotor): add enum for all rotor types and refactor code

--- src/modules/enigma/rotor.rs
+++ src/modules/enigma/rotor.rs
@@ -1,5 +1,43 @@
 use std::char;
 
+pub enum RotorType {
+    I,
+    II,
+    III,
+    IV,
+    V,
+    VI,
+    VII,
+    VIII,
+    Beta,
+    Gamma,
+    ReflectorB,
+    ReflectorC,
+    ReflectorBDunn,
+    ReflectorCDunn,
+}
+
+impl RotorType {
+    pub fn to_string(&self) -> String {
+        match self {
+            RotorType::I => "EKMFLGDQVZNTOWYHXUSPAIBRCJ".to_string(),
+            RotorType::II => "AJDKSIRUXBLHWTMCQGZNPYFVOE".to_string(),
+            RotorType::III => "BDFHJLCPRTXVZNYEIWGAKMUSQO".to_string(),
+            RotorType::IV => "ESOVPZJAYQUIRHXLNFTGKDCMWB".to_string(),
+            RotorType::V => "VZBRGITYUPSDNHLXAWMJQOFECK".to_string(),
+            RotorType::VI => "JPGVOUMFYQBENHZRDKASXLICTW".to_string(),
+            RotorType::VII => "NZJHGRCXMYSWBOUFAIVLPEKQDT".to_string(),
+            RotorType::VIII => "FKQHTLXOCBJSPDZRAMEWNIUYGV".to_string(),
+            RotorType::Beta => "LEYJVCNIXWPBQMDRTAKZGFUHOS".to_string(),
+            RotorType::Gamma => "FSOKANUERHMBTIYCWLQPZXVGJD".to_string(),
+            RotorType::ReflectorB => "YRUHQSLDPXNGOKMIEBFZCWVJAT".to_string(),
+            RotorType::ReflectorC => "FVPJIAOYEDRZXWGCTKUQSBNMHL".to_string(),
+            RotorType::ReflectorBDunn => "ENKQAUYWJICOPBLMDXZVFTHRGS".to_string(),
+            RotorType::ReflectorCDunn => "RDOBJNTKVEHMLFCWZAXGYIPSUQ".to_string(),
+        }
+    }
+}
+
 pub struct Rotor {
     shift: i32,
     shift_count: i32,
@@ -8,47 +46,40 @@ pub struct Rotor {
 }
 
 impl Rotor {
-    pub fn new(alphabet: String, shift: i32, start_position: i32) -> Self {
+    pub fn new(rotor_type: RotorType, shift: i32, start_position: i32) -> Self {
         Self {
             shift_count: start_position,
             shift,
-            alphabet: alphabet.to_uppercase(),
+            alphabet: rotor_type.to_string(),
             initial_alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string(),
         }
     }
     pub fn get(&self, char: char) -> char {
-        let char = char.to_ascii_uppercase();
-        let position = self.initial_alphabet.chars().position(|x| x == char);
-
-        match position {
-            Some(pos) => {
-                let index = (pos as i32) + self.shift_count;
-
-                self.alphabet
-                    .chars()
-                    .nth(index.rem_euclid(self.alphabet.len() as i32) as usize)
-                    .expect("[Rotor] - Index out of bounds")
-            }
-            None => char,
-        }
+        self.get_shifted(char, self.shift_count, &self.initial_alphabet, &self.alphabet)
     }
 
     pub fn get_back(&self, char: char) -> char {
-        let char = char.to_ascii_uppercase();
-        let position = self.alphabet.chars().position(|x| x == char);
-        match position {
-            Some(pos) => {
-                let index = (pos as i32) - self.shift_count;
+        self.get_shifted(char, -self.shift_count, &self.alphabet, &self.initial_alphabet)
+    }
 
-                self.initial_alphabet
+    fn get_shifted(&self, char: char, shift: i32, source: &str, target: &str) -> char {
+        let char = char.to_ascii_uppercase();
+
+        match source.chars().position(|x| x == char) {
+            Some(pos) => {
+                let index = (pos as i32) - shift;
+
+                target
                     .chars()
-                    .nth(index.rem_euclid(self.initial_alphabet.len() as i32) as usize)
+                    .nth(index.rem_euclid(target.len() as i32) as usize)
                     .expect("[Rotor] - Index out of bounds")
             }
             None => char,
         }
     }
+
     pub fn rotate(&mut self) {
-        self.shift_count += self.shift;
+        //add rem euclid to avoid overflow
+        self.shift_count = (self.shift + self.shift_count).rem_euclid(26);
     }
 }
