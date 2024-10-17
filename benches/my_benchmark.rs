5ca48524124a9e7af5f98aebcbb5a70271bcef99
kovalev Andrey <andremae10@gmail.com>
2024-10-17 23:41:27 +0300

    feat(benchmark): add route and double permutations benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,9 @@
 extern crate criterion;
 use criterion::{ black_box, criterion_group, criterion_main, Criterion };
-use primeculator::modules::substitution_ciphers::{ caesars, trisemus };
+use primeculator::modules::{
+    substitution_ciphers::{ caesars, trisemus },
+    transposition_ciphers::{ double_permutation_encrypt, route_permutation_encrypt },
+};
 
 fn benchmark_caesars(c: &mut Criterion) {
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -48,5 +51,46 @@ fn benchmark_trisemus(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, benchmark_caesars, benchmark_trisemus);
+fn benchmark_route_permutation(c: &mut Criterion) {
+    let sizes = vec![10, 100, 1000, 10000, 100000];
+
+    let mut group = c.benchmark_group("route_permutation");
+    for &size in &sizes {
+        group.bench_function(format!("route_permutation_{}", size), |b| {
+            let text = "A".repeat(size);
+            b.iter(|| {
+                let result = route_permutation_encrypt(black_box(text.clone()));
+                black_box(result);
+            })
+        });
+    }
+    group.finish();
+}
+
+fn benchmark_double_permutation(c: &mut Criterion) {
+    let sizes = vec![10, 100, 1000, 10000, 100000];
+
+    let mut group = c.benchmark_group("double_permutation");
+    for &size in &sizes {
+        group.bench_function(format!("double_permutation_{}", size), |b| {
+            let text = "A".repeat(size);
+            b.iter(|| {
+                let result = double_permutation_encrypt(
+                    black_box(text.clone()),
+                    black_box("1320".to_string()),
+                    black_box("423051".to_string())
+                );
+                black_box(result);
+            })
+        });
+    }
+    group.finish();
+}
+criterion_group!(
+    benches,
+    benchmark_caesars,
+    benchmark_trisemus,
+    benchmark_route_permutation,
+    benchmark_double_permutation
+);
 criterion_main!(benches);
