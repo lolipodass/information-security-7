0b68fdb5adbb757cdddb5dbed428944bfe3668cf
kovalev Andrey <andremae10@gmail.com>
2024-10-26 17:13:54 +0300

    feat(number utils): add compute extended euclidean  algorithm and modular multiplicative inverse calculation

--- src/modules/number_utils.rs
+++ src/modules/number_utils.rs
@@ -27,3 +27,47 @@ pub fn find_prime_numbers(a: i32, b: i32) -> Vec<i32> {
 
     res
 }
+
+pub fn mod_inverse(a: u128, n: u128) -> Option<u128> {
+    let (gcd, x, _) = gcd_bezout(a as i128, n as i128);
+    if gcd == 1 {
+        Some(x.rem_euclid(n as i128) as u128)
+    } else {
+        None
+    }
+}
+
+///returns (gcd, x, y)
+///
+///where a * x + b * y = gcd
+pub fn gcd_bezout(a: i128, b: i128) -> (i128, i128, i128) {
+    let mut x = 1;
+    let mut y = 0;
+    let mut x1 = 0;
+    let mut y1 = 1;
+
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let q = a / b;
+        (x, x1) = (x1, x - q * x1);
+        (y, y1) = (y1, y - q * y1);
+        (a, b) = (b, a - q * b);
+    }
+
+    (a, x, y)
+}
+
+#[test]
+fn test_gcd_bezout() {
+    assert_eq!(gcd_bezout(10, 15), (5, -1, 1));
+    assert_eq!(gcd_bezout(31, 17), (1, -6, 11));
+    assert_eq!(gcd_bezout(3, 10), (1, -3, 1));
+}
+
+#[test]
+fn test_mod_inverse() {
+    assert_eq!(mod_inverse(3, 11), Some(4));
+    assert_eq!(mod_inverse(3, 10), Some(7));
+    assert_eq!(mod_inverse(3, 9), None);
+}
