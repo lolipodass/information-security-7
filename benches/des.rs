ed5a852bbfd82cec6ed783ee57b0e968e5ff9d84
kovalev Andrey <andremae10@gmail.com>
2024-10-28 11:36:32 +0300

    feat(benchmark): add decrypting des benchmark

--- benches/des.rs
+++ benches/des.rs
@@ -1,15 +1,34 @@
 use criterion::{ black_box, Criterion };
-use primeculator::modules::des::des::encrypt_des;
+use primeculator::modules::des::des::{ decrypt_des, encrypt_des };
 
-pub fn des_benchmark(c: &mut Criterion) {
+pub fn des_encrypt_benchmark(c: &mut Criterion) {
     let sizes = vec![10, 100, 1000, 10000, 100000];
 
-    let mut group = c.benchmark_group("des_cipher");
+    let mut group = c.benchmark_group("des_encrypt_cipher");
     for &size in &sizes {
-        group.bench_function(format!("des_cipher_{}", size), |b| {
+        group.bench_function(format!("des_encrypt_cipher_{}", size), |b| {
             let text = "A".repeat(size);
             b.iter(|| {
                 let result = encrypt_des(
+                    &text.as_bytes().to_vec(),
+                    &vec![0x1f, 0x5a, 0x1f, 0x5a, 0x1f, 0x5a, 0x1f, 0x5a]
+                );
+                black_box(result);
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn des_decrypt_benchmark(c: &mut Criterion) {
+    let sizes = vec![10, 100, 1000, 10000, 100000];
+
+    let mut group = c.benchmark_group("des_decrypt_cipher");
+    for &size in &sizes {
+        group.bench_function(format!("des_decrypt_cipher_{}", size), |b| {
+            let text = "A".repeat(size);
+            b.iter(|| {
+                let result = decrypt_des(
                     &text.as_bytes().to_vec(),
                     &vec![0x1f, 0x5a, 0x1f, 0x5a, 0x1f, 0x5a, 0x1f, 0x5a]
                 );
--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,7 +2,7 @@ extern crate criterion;
 use criterion::{ criterion_group, criterion_main };
 
 use caesars::benchmark_caesars;
-use des::des_benchmark;
+use des::{ des_decrypt_benchmark, des_encrypt_benchmark };
 use double_permutation::benchmark_double_permutation;
 use enigma::enigma_benchmark;
 use rc4::rc4_benchmark;
@@ -24,7 +24,8 @@ criterion_group!(
     benchmark_trisemus,
     benchmark_route_permutation,
     benchmark_double_permutation,
-    des_benchmark,
+    des_decrypt_benchmark,
+    des_encrypt_benchmark,
     rc4_benchmark
 );
 criterion_main!(benches);
