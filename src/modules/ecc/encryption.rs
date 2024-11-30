b3e3be7f1fb55aecf3ab22fb663cb2d3d297f216
kovalev Andrey <andremae10@gmail.com>
2024-11-30 11:16:07 +0300

    feat(ecc): add elliptic curve text encryption
    add function to convert u8 to point and vice versa

--- src/modules/ecc/encryption.rs
+++ src/modules/ecc/encryption.rs
@@ -5,6 +5,9 @@ use crate::modules::number_utils::mod_inverse;
 
 use super::{ curve::EpilepticCurve, point::Point };
 
+const FIRST_FACTOR: u8 = 11;
+const SECOND_FACTOR: u8 = 13;
+
 pub struct ECCEncryption {
     curve: EpilepticCurve,
     generator: Point,
@@ -20,6 +23,49 @@ impl ECCEncryption {
         let d = thread_rng().gen_range(2..order - 1);
         let y = curve.scalar(generator, d);
         Self { curve, generator, d, order, open_key: y }
+    }
+
+    pub fn encrypt_text_elgamal(&self, message: &[u8]) -> Vec<(Point, Point)> {
+        let mut res = Vec::new();
+
+        for letter in message {
+            let (c1, c2) = self.encrypt_point_elgamal(self.convert_byte_to_point(*letter));
+            res.push((c1, c2));
+        }
+
+        res
+    }
+
+    pub fn decrypt_text_elgamal(&self, message: &[(Point, Point)]) -> Vec<u8> {
+        let mut res = Vec::new();
+
+        for (c1, c2) in message {
+            let point = self.decrypt_point_elgamal(*c1, *c2);
+            let byte = self.convert_point_to_byte(point);
+            res.push(byte);
+        }
+
+        res
+    }
+
+    fn convert_byte_to_point(&self, bytes: u8) -> Point {
+        let low = bytes & 0b0000_1111;
+        let high = (bytes >> 4) & 0b0000_1111;
+        let x = low * FIRST_FACTOR + SECOND_FACTOR;
+        let y = high * SECOND_FACTOR + FIRST_FACTOR;
+        Point::Finite { x: x as i64, y: y as i64 }
+    }
+
+    fn convert_point_to_byte(&self, point: Point) -> u8 {
+        if let Point::Finite { x, y } = point {
+            let mut low = x as u8;
+            low = (low - SECOND_FACTOR) / FIRST_FACTOR;
+            let mut high = y as u8;
+            high = (high - FIRST_FACTOR) / SECOND_FACTOR;
+
+            return low | (high << 4);
+        }
+        return 0;
     }
 
     pub fn encrypt_point_elgamal(&self, message: Point) -> (Point, Point) {
@@ -142,4 +188,29 @@ fn test_ecc_signature() {
 
     signature.0 += 1;
     assert!(!crypt.verify(message, signature));
+}
+
+#[test]
+fn test_point_conversions() {
+    let crypt = ECCEncryption::new(-1, 1, 751, Point::Finite { x: 0, y: 1 });
+    let char = b'a';
+    println!("char: {:08b}", char);
+    let point = crypt.convert_byte_to_point(char);
+    println!("point: {}", point);
+    let char = crypt.convert_point_to_byte(point);
+    println!("char: {}", char);
+    assert_eq!(char, char);
+}
+
+#[test]
+fn test_ecc_encryption_text() {
+    let crypt = ECCEncryption::new(-1, 1, 751, Point::Finite { x: 0, y: 1 });
+
+    let message = b"Hello, world!";
+
+    let encrypted = crypt.encrypt_text_elgamal(message);
+
+    let decrypted = crypt.decrypt_text_elgamal(&encrypted);
+
+    assert_eq!(decrypted, message);
 }
